8a043a933de021b7257dce452e2d3c76bdeaab2c
Rudi Martinsen <rudi.martinsen@gmail.com>
2019-03-28 14:37:44 +0100

    Adding functions for updating and removing appliances

---
+++ GlobalDashboardPS/Public/Remove-OVGDAppliance.ps1
@@ -0,0 +1,27 @@
+function Remove-OVGDAppliance {
+    <#
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        $Server,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [alias("Appliance")]
+        $Entity
+    )
+    BEGIN {
+        $ResourceType = "appliances"
+
+    }
+    PROCESS {
+        
+        $Resource = BuildPath -Resource $ResourceType -Entity $Entity
+
+        if ($PSCmdlet.ShouldProcess("ShouldProcess?")) {
+            Invoke-OVGDRequest -Resource $Resource -Method DELETE
+        }
+    }
+    END {
+
+    }
+
+}
---
+++ GlobalDashboardPS/Public/Set-OVGDAppliance.ps1
@@ -0,0 +1,70 @@
+function Set-OVGDAppliance {
+    <#
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        $Server,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [alias("Appliance")]
+        $Entity,
+        [Parameter(ParameterSetName="Name")]
+        $NewName,
+        [Parameter(ParameterSetName="Refresh")]
+        [switch]
+        $Refresh,
+        [Parameter(ParameterSetName="Reconnect")]
+        [switch]
+        $Reconnect,
+        [Parameter(ParameterSetName="Reconnect")]
+        $Username,
+        [Parameter(ParameterSetName="Reconnect")]
+        [securestring]
+        $Password,
+        [Parameter(ParameterSetName="Reconnect")]
+        $LoginDomain
+    )
+    BEGIN {
+        $ResourceType = "appliances"
+        $operation = "replace"
+    }
+    PROCESS {
+
+        if ($Reconnect -and !$Password) {
+            $Password = Read-Host -Prompt "Password please" -AsSecureString
+            $unsecPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password))
+            $value = [PSCustomObject]@{
+                username = $Username
+                password = $unsecPass
+                loginDomain = $LoginDomain
+            } #| ConvertTo-Json
+            $opPath = "/credential"
+        }
+        elseif($Refresh) {
+            $value = "refreshPending"
+            $opPath = "/status"
+        }
+        else {
+            $value = $NewName
+            $opPath = "/applianceName"
+        }
+        
+        $abody = [PSCustomObject]@{
+            op = $operation
+            path = $opPath
+            value = $value
+        } #| ConvertTo-Json
+        
+        $body = ConvertTo-Json @($abody)
+        Write-Verbose $body
+        
+        $Resource = BuildPath -Resource $ResourceType -Entity $Entity
+
+        if ($PSCmdlet.ShouldProcess("ShouldProcess?")) {
+            Invoke-OVGDRequest -Resource $Resource -Method PATCH -Body $body -ContentType "application/json-patch+json" -Verbose
+        }
+    }
+    END {
+
+    }
+
+}
