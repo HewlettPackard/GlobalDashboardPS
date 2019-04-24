1063b0860ed6e3c41b922c299e6d011f681e68e7
Rudi Martinsen <rudi.martinsen@gmail.com>
2019-04-24 18:41:36 +0200

    Adding support for querying, ref #9, changed warning text on result count greater than count param

--- GlobalDashboardPS/Public/Get-OVGDAppliance.ps1
+++ GlobalDashboardPS/Public/Get-OVGDAppliance.ps1
@@ -8,9 +8,10 @@ function Get-OVGDAppliance {
             Info
             Author : Rudi Martinsen / Intility AS
             Date : 25/03-2019
-            Version : 0.2.2
+            Version : 0.3.0
             Revised : 24/04-2019
             Changelog:
+            0.3.0 -- Added support for querying, changed text when result is bigger than count
             0.2.2 -- Fixed minor bug in help text, added link
             0.2.1 -- Added help text
             0.2.0 -- Added count param
@@ -24,6 +25,16 @@ function Get-OVGDAppliance {
             The Global Dashboard to retrieve appliances from
         .PARAMETER Entity
             The appliance to retrieve
+        .PARAMETER ApplianceName
+            Filter on ApplianceName of Appliance to retrieve. Note the search is case-sensitive and searches for an exact match
+        .PARAMETER ApplianceLocation
+            Filter on ApplianceLocation of Appliance to retrieve. Note the search is case-sensitive and searches for an exact match
+        .PARAMETER Status
+            Filter on Status of Appliance to retrieve. Note the search is case-sensitive and searches for an exact match
+        .PARAMETER State
+            Filter on State of Appliance to retrieve. Note the search is case-sensitive and searches for an exact match
+        .PARAMETER UserQuery
+            Query string used for full text search
         .PARAMETER Count
             The count of appliances to retrieve, defaults to 25
         .EXAMPLE
@@ -35,12 +46,31 @@ function Get-OVGDAppliance {
 
             Retrieves the specific OneView appliances with the name "oneview-001"
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Default")]
     param (
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
         $Server,
         [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+        [Parameter(ParameterSetName="Id")]
         [alias("Appliance")]
         $Entity,
+        [Parameter(ParameterSetName="Query")]
+        $ApplianceName,
+        [Parameter(ParameterSetName="Query")]
+        $ApplianceLocation,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("OK","Warning","Critical")]
+        $Status,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("Online","Offline","Failed","Unknown")]
+        $State,
+        [Parameter(ParameterSetName="Query")]
+        $UserQuery,
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
         $Count = 25
     )
     BEGIN {
@@ -49,11 +79,45 @@ function Get-OVGDAppliance {
     }
     PROCESS {
         $Resource = BuildPath -Resource $ResourceType -Entity $Entity
+        $Query = "count=$Count"
+        $searchFilters = @()
+        $txtSearchFilters = @()
 
-        $result = Invoke-OVGDRequest -Resource $Resource
+        if($ApplianceName){
+            $searchFilters += 'applianceName EQ "' + $ApplianceName + '"'
+        }
 
+        if($ApplianceLocation){
+            $searchFilters += 'applianceLocation EQ "' + $ApplianceLocation + '"'
+        }
+
+        if($Status){
+            $searchFilters += 'status EQ "' + $Status + '"'
+        }
+
+        if($State){
+            $searchFilters += 'state EQ "' + $State + '"'
+        }
+
+        if($UserQuery){
+            $txtSearchFilters += "$UserQuery"
+        }
+
+        if($searchFilters){
+            $filterQry = $searchFilters -join " AND "
+            $Query += '&query="' + $filterQry + '"'
+        }
+
+        if($txtSearchFilters){
+            $filterQry = $txtSearchFilters -join " AND "
+            $Query += '&userQuery="' + $filterQry + '"'
+        }
+
+        $result = Invoke-OVGDRequest -Resource $Resource -Query $Query
+
+        Write-Verbose "Got a total of $($result.total) result(s)"
         if ($result.Count -lt $result.Total ) {
-            Write-Verbose "The result has been paged. Total number of results is: $($result.total)"
+            Write-Warning "The result has been paged. Total number of results is: $($result.total)"
         }
 
         $output = Add-OVGDTypeName -TypeName "GlobalDashboardPS.OVGDAppliance" -Object $result.members
