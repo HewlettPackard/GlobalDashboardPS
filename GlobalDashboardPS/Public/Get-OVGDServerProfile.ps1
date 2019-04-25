090a30e5e605da58a9e512abceda6a799b760c1d
Rudi Martinsen <rudi.martinsen@gmail.com>
2019-04-25 08:27:59 +0200

    Added support for querying, ref #9, fixed warning when total result is bigger than count

--- GlobalDashboardPS/Public/Get-OVGDServerProfile.ps1
+++ GlobalDashboardPS/Public/Get-OVGDServerProfile.ps1
@@ -8,9 +8,10 @@ function Get-OVGDServerProfile {
             Info
             Author : Rudi Martinsen / Intility AS
             Date : 25/03-2019
-            Version : 0.2.2
-            Revised : 24/04-2019
+            Version : 0.3.0
+            Revised : 25/04-2019
             Changelog:
+            0.3.0 -- Added support for querying and changed warning when result is bigger than count
             0.2.2 -- Fixed bug in help text and added link
             0.2.1 -- Added help text
             0.2.0 -- Added count param
@@ -24,8 +25,22 @@ function Get-OVGDServerProfile {
             The Global Dashboard to retrieve Server Profiles from
         .PARAMETER Entity
             The Id of the Server Profile to retrieve
+        .PARAMETER ServerName
+            Filter on the ServerName of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER SerialNumber
+            Filter on the SerialNumber of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER Appliance
+            Filter on the Appliance of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER Status
+            Filter on the Status of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER State
+            Filter on State of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER TemplateCompliance
+            Filter on the Template Compliance of the Server Profile to retrieve. Note that we search for an exact match
+        .PARAMETER UserQuery
+            Query string used for full text search
         .PARAMETER Count
-            The count of profiles to retrieve, defaults to 25
+            The count of Server Profile to retrieve, defaults to 25
         .EXAMPLE
             PS C:\> Get-OVGDServerProfile
 
@@ -34,12 +49,46 @@ function Get-OVGDServerProfile {
             PS C:\> Get-OVGDServerProfile -Entity xxxxxxxx-xxxx-xxxx-xxxx-54e195f27f36
 
             Retrieves the specific Server Profile with the specified ID
+        .EXAMPLE
+            PS C:\> Get-OVGDServerProfile -ServerName my-server-name-001
+
+            Searches for a Server Profile associated with the specified servername
+        .EXAMPLE
+            PS C:\> Get-OVGDServerProfile -UserQuery "Proliant DL360"
+
+            Performs a full text search and matches all attributes for the specified string
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Default")]
     param (
-        $Server,
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
+        $Server = $Global:OVGDPSServer,
+        [Parameter(ParameterSetName="Id")]
         [alias("ServerProfile")]
         $Entity,
+        [Parameter(ParameterSetName="Query")]
+        $ServerProfileName,
+        [Parameter(ParameterSetName="Query")]
+        $Appliance,
+        [Parameter(ParameterSetName="Query")]
+        $SerialNumber,
+        [Parameter(ParameterSetName="Query")]
+        $ServerName,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("Compliant","NonCompliant","Unknown")]
+        $TemplateCompliance,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("OK","Warning","Critical","Disabled","Unknown")]
+        $Status,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("Normal","CreateFailed","UpdateFailed","Unknown")]
+        $State,
+        [Parameter(ParameterSetName="Query")]
+        $UserQuery,
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
         $Count = 25
     )
 
@@ -50,10 +99,56 @@ function Get-OVGDServerProfile {
     process {
         $Resource = BuildPath -Resource $ResourceType -Entity $Entity
         $Query = "count=$Count"
-        $result = Invoke-OVGDRequest -Resource $Resource -Query $Query
+        $searchFilters = @()
+        $txtSearchFilters = @()
+        
+        if($ServerProfileName){
+            $searchFilters += 'name EQ "' + $ServerProfileName + '"'
+        }
 
+        if($SerialNumber){
+            $searchFilters += 'serialNumber EQ "' + $SerialNumber + '"'
+        }
+
+        if($Appliance){
+            $searchFilters += 'applianceName EQ "' + $Appliance + '"'
+        }
+
+        if($ServerName){
+            $searchFilters += 'serverName EQ "' + $ServerName + '"'
+        }
+
+        if($TemplateCompliance){
+            $searchFilters += 'templateCompliance EQ "' + $TemplateCompliance + '"'
+        }
+
+        if($Status){
+            $searchFilters += 'status EQ "' + $Status + '"'
+        }
+
+        if($State){
+            $searchFilters += 'state EQ "' + $State + '"'
+        }
+
+        if($UserQuery){
+            $txtSearchFilters += "$UserQuery"
+        }
+
+        if($searchFilters){
+            $filterQry = $searchFilters -join " AND "
+            $Query += '&query="' + $filterQry + '"'
+        }
+
+        if($txtSearchFilters){
+            $filterQry = $txtSearchFilters -join " AND "
+            $Query += '&userQuery="' + $filterQry + '"'
+        }
+
+        $result = Invoke-OVGDRequest -Resource $Resource -Query $Query #-Verbose
+
+        Write-Verbose "Found $($result.total) number of results"
         if ($result.Count -lt $result.Total ) {
-            Write-Verbose "The result has been paged. Total number of results is: $($result.total)"
+            Write-Warning "The result has been paged. Total number of results is: $($result.total)"
         }
 
         $output = Add-OVGDTypeName -TypeName "GlobalDashboardPS.OVGDServerProfile" -Object $result.members
