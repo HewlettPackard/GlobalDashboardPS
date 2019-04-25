1b4f0fcc4c9fe5fedc5266451c927677c09c6bff
Rudi Martinsen <rudi.martinsen@gmail.com>
2019-04-25 08:32:26 +0200

    Added support for querying, ref #9, fixed warning when total result is bigger than count

--- GlobalDashboardPS/Public/Get-OVGDServerProfileTemplate.ps1
+++ GlobalDashboardPS/Public/Get-OVGDServerProfileTemplate.ps1
@@ -8,9 +8,10 @@ function Get-OVGDServerProfileTemplate {
             Info
             Author : Rudi Martinsen / Intility AS
             Date : 25/03-2019
-            Version : 0.2.2
-            Revised : 24/04-2019
+            Version : 0.3.0
+            Revised : 25/04-2019
             Changelog:
+            0.3.0 -- Added support for querying and changed warning when result is bigger than count
             0.2.2 -- Fixed bug in help text and added link
             0.2.1 -- Added help text
             0.2.0 -- Added count param
@@ -24,6 +25,16 @@ function Get-OVGDServerProfileTemplate {
             The Global Dashboard to retrieve Server Profile Templates from
         .PARAMETER Entity
             The Id of the Server Profile Template to retrieve
+        .PARAMETER TemplateName
+            Filter on the Name of the Server Profile Template to retrieve. Note that we search for an exact match
+        .PARAMETER Appliance
+            Filter on the Appliance of the Server Profile Template to retrieve. Note that we search for an exact match
+        .PARAMETER Status
+            Filter on the Status of the Server Profile Template to retrieve. Note that we search for an exact match
+        .PARAMETER State
+            Filter on State of the Server Profile Template to retrieve. Note that we search for an exact match
+        .PARAMETER UserQuery
+            Query string used for full text search
         .PARAMETER Count
             The count of profile templates to retrieve, defaults to 25
         .EXAMPLE
@@ -35,11 +46,30 @@ function Get-OVGDServerProfileTemplate {
 
             Retrieves the specific Server Profile Template with the specified ID
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Default")]
     param (
-        $Server,
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
+        $Server = $Global:OVGDPSServer,
+        [Parameter(ParameterSetName="Id")]
         [alias("ServerProfileTemplate")]
         $Entity,
+        [Parameter(ParameterSetName="Query")]
+        $TemplateName,
+        [Parameter(ParameterSetName="Query")]
+        $Appliance,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("OK","Warning","Critical","Disabled","Unknown")]
+        $Status,
+        [Parameter(ParameterSetName="Query")]
+        [ValidateSet("Normal","CreateFailed","UpdateFailed","Unknown")]
+        $State,
+        [Parameter(ParameterSetName="Query")]
+        $UserQuery,
+        [Parameter(ParameterSetName="Default")]
+        [Parameter(ParameterSetName="Id")]
+        [Parameter(ParameterSetName="Query")]
         $Count = 25
     )
 
@@ -50,10 +80,44 @@ function Get-OVGDServerProfileTemplate {
     process {
         $Resource = BuildPath -Resource $ResourceType -Entity $Entity
         $Query = "count=$Count"
-        $result = Invoke-OVGDRequest -Resource $Resource -Query $Query -Verbose
+        $searchFilters = @()
+        $txtSearchFilters = @()
+        
+        if($TemplateName){
+            $searchFilters += 'name EQ "' + $TemplateName + '"'
+        }
 
+        if($Appliance){
+            $searchFilters += 'applianceName EQ "' + $Appliance + '"'
+        }
+
+        if($Status){
+            $searchFilters += 'status EQ "' + $Status + '"'
+        }
+
+        if($State){
+            $searchFilters += 'state EQ "' + $State + '"'
+        }
+
+        if($UserQuery){
+            $txtSearchFilters += "$UserQuery"
+        }
+
+        if($searchFilters){
+            $filterQry = $searchFilters -join " AND "
+            $Query += '&query="' + $filterQry + '"'
+        }
+
+        if($txtSearchFilters){
+            $filterQry = $txtSearchFilters -join " AND "
+            $Query += '&userQuery="' + $filterQry + '"'
+        }
+
+        $result = Invoke-OVGDRequest -Resource $Resource -Query $Query #-Verbose
+
+        Write-Verbose "Found $($result.total) number of results"
         if ($result.Count -lt $result.Total ) {
-            Write-Verbose "The result has been paged. Total number of results is: $($result.total)"
+            Write-Warning "The result has been paged. Total number of results is: $($result.total)"
         }
 
         $output = Add-OVGDTypeName -TypeName "GlobalDashboardPS.OVGDServerProfileTemplate" -Object $result.members
